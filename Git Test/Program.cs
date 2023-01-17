aa2c24eef6d8040615a29785b0cd272cd7463e2f
BrightS0le <hunmjohn@ut.utm.edu>
2023-01-17 14:21:29 -0600

    does x and y

--- Git Test/Program.cs
+++ Git Test/Program.cs
@@ -10,7 +10,15 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
+            string v;
             Console.WriteLine("Please kill me, *cough* *cough* I mean Hello World!");
+            Console.WriteLine("I am not smart enough for this...");
+            Console.WriteLine("For the next values please enter a lower and upper bound for a number guessing game respectively...");
+            Console.Write("Enter the value for x:");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter the value for y:");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("X:" + x + " Y:" + y);
             Console.ReadKey();
         }
     }
